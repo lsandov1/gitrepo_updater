1b0b5ac56a1bda0e5950f434e0283a75da0b61d0
Leonardo Sandoval <leonardo@ooyala.com>
2012-05-16 17:46:09 -0500

    use lib as a test script

--- github.rb
+++ github.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'octokit'
+require 'trollop'
 
 class GithubAccount
   def initialize(login, pwd)
@@ -47,7 +48,7 @@ class GithubAccount
     list_hooks(user, repo_name).include? hook_url
   end
 
-  def test_hook(user, repo_name, hook_url='http://csg-eng.ooyala.com/puller')
+  def test_hook(user, repo_name, hook_url)
     hooks = list_hooks(user, repo_name)
     if hooks[hook_url]
       id = hooks[hook_url]
@@ -56,3 +57,19 @@ class GithubAccount
     end
   end
 end
+
+if (__FILE__ == $0) then
+  opts = Trollop::options do
+    opt :github_user_name, 'The Github username', :default => 'ooyala'
+    opt :github_repository_name, 'The Github repository name', :default=>'', :required => true
+    opt :local_path, 'The local Github repository path', :default => '', :required => true
+    opt :restart_cmd, 'Command line to restart your app', :default => ''
+    opt :github_login, 'The Github login', :default => '', :required => true
+    opt :github_password, 'The Github password', :default => '', :required => true
+    opt :github_hook, 'The Github hook, which is the name of the current domain', :default => '', :required => true
+  end
+  account = GithubAccount(opts[:github_login,:github_password])
+  account.test_hook(opts[:github_user_name,
+                    opts[:github_repository_name],
+                    opts[:github_hook])
+ end 
